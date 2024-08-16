94167b87e07d481a588bc376842e1b093c275aba
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-16 11:22:56 +0300

    feat: init first window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use raylib::prelude::*;
 
+static WINDOW_WIDTH: i32 = 680;
+static WINDOW_HEIGHT: i32 = 440;
+
 fn main() {
-    todo!();
+    let (mut lib, thread) = raylib::init()
+        .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        .title("My Pomodoro")
+        .build();
+
+    while !lib.window_should_close() {
+        let mut draw = lib.begin_drawing(&thread);
+        draw.clear_background(Color::BLACK);
+    }
 }
