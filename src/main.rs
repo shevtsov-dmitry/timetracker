5e370c10325f86161507fa67ef25a57cbed036a4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-08-16 11:28:31 +0300

    feat: add border for button

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,22 @@ fn main() {
 fn draw_button(mut draw: RaylibDrawHandle) {
     let button_width = WINDOW_WIDTH / 5;
     let button_height = WINDOW_HEIGHT / 8;
+    let border_len = 3;
+
+    let btn = Rectangle::new(
+        (WINDOW_WIDTH / 2 - button_width / 2) as f32,
+        (WINDOW_HEIGHT - WINDOW_HEIGHT / 6) as f32,
+        (button_width) as f32,
+        (button_height) as f32,
+    );
 
     let border = Rectangle::new(
-        (WINDOW_WIDTH / 2 - button_width / 2) as f32,
-        (WINDOW_HEIGHT / 2) as f32,
-        (button_width) as f32,
-        (button_height) as f32,
+        (btn.x as i32 - border_len) as f32,
+        (btn.y as i32 - border_len) as f32,
+        (btn.width as i32 + border_len * 2) as f32,
+        (btn.height as i32 + border_len * 2) as f32,
     );
+
     draw.draw_rectangle_rounded(border, 0.2, 1, Color::WHITE);
-
-    let rec = Rectangle::new(
-        (WINDOW_WIDTH / 2 - button_width / 2) as f32,
-        (WINDOW_HEIGHT / 2) as f32,
-        (button_width) as f32,
-        (button_height) as f32,
-    );
-
-    draw.draw_rectangle_rounded(rec, 0.2, 1, Color::REBECCAPURPLE);
+    draw.draw_rectangle_rounded(btn, 0.2, 1, Color::REBECCAPURPLE);
 }
