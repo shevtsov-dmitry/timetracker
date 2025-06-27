705d625b98f0293f439d35aedcc8e2ca1568ddc3
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-06-27 21:58:43 +0300

    refactor: use tauri for client, docker + kotlin for backend

---
+++ server/app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,14 @@
+package org.example
+
+class App {
+
+    fun returnTwoTimesTwelveInString(): String {
+        val answer = 2 * 12
+        return "$answer"
+    }
+
+}
+
+fun main() {
+    println("Ищи")
+}
